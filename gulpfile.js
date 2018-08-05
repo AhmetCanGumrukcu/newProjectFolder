ebd261d184d40b7dcd4fb2a6aac4d81977c69427
AhmetCanGumrukcu <parsdizayn@gmail.com>
2018-08-05 08:54:32 +0300

    Gulp file update
    add browsersync and htaccess

--- gulpfile.js
+++ gulpfile.js
@@ -7,13 +7,15 @@ var watch = require('gulp-watch');
 var sourcemaps = require("gulp-sourcemaps");
 var pug = require('gulp-pug');
 var plumber = require('gulp-plumber');
+var browserSync = require('browser-sync').create();
 
 gulp.task('sass', function () {
     return gulp.src('./assets/scss/*.scss')
         .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest('./assets/css'));
+        .pipe(gulp.dest('./assets/css'))
+        .pipe(browserSync.stream());
 });
 
 
@@ -27,6 +29,7 @@ gulp.task('scripts', function () {
 });
 */
 
+
 gulp.task('views', function buildHTML() {
     return gulp.src('./views/!(_)*.pug')
     .pipe(plumber())
@@ -35,9 +38,14 @@ gulp.task('views', function buildHTML() {
   });
 
 gulp.task('watch', function () {
+
+    browserSync.init({
+        server: "./"
+    });
     
     gulp.watch('./assets/scss/*.scss', ['sass']);
-    gulp.watch('./views/**/*.pug',['views']);
+    gulp.watch("./assets/js/*.js").on('change', browserSync.reload);;
+    gulp.watch('./views/**/*.pug',['views']).on('change', browserSync.reload);
 
 });
 
