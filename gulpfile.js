c9ae79ea8a31806da47ea6f0b320d9f1c5ff54d5
AhmetCanGumrukcu <parsdizayn@gmail.com>
2018-08-06 14:57:41 +0300

    Css min and Js concat update
    add gulp file css.min and js concat

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,15 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var watch = require('gulp-watch');
-//var concat = require("gulp-concat");
+var concat = require("gulp-concat");
 var sourcemaps = require("gulp-sourcemaps");
 var pug = require('gulp-pug');
 var plumber = require('gulp-plumber');
 var browserSync = require('browser-sync').create();
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
 
+// sass
 gulp.task('sass', function () {
     return gulp.src('./assets/scss/*.scss')
         .pipe(sourcemaps.init())
@@ -18,18 +21,27 @@ gulp.task('sass', function () {
         .pipe(browserSync.stream());
 });
 
-
-/*
-gulp.task('scripts', function () {
-    return gulp.src('./js/*.js')
-        .pipe(sourcemaps.init())
-        .pipe(concat('all.js'))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('./js/'));
+// css minify
+gulp.task('cssMin', function () {
+    gulp.src('./assets/css/*.css')
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('./assets/css'));
 });
-*/
 
 
+// concat js
+gulp.task('scripts', function () {
+    return gulp.src(['./assets/libs/js/jquery.min.js','./assets/libs/js/bootstrap.min.js'])
+        .pipe(sourcemaps.init())
+        .pipe(concat('app.js'))
+        .pipe(sourcemaps.write())
+        .pipe(rename('app.min.js'))
+        .pipe(gulp.dest('./assets/js'));
+});
+
+
+// pug
 gulp.task('views', function buildHTML() {
     return gulp.src('./views/!(_)*.pug')
     .pipe(plumber())
@@ -37,6 +49,7 @@ gulp.task('views', function buildHTML() {
     .pipe(gulp.dest("./"))
   });
 
+  //watch
 gulp.task('watch', function () {
 
     browserSync.init({
@@ -44,9 +57,12 @@ gulp.task('watch', function () {
     });
     
     gulp.watch('./assets/scss/*.scss', ['sass']);
+    gulp.watch('./assets/css/*.css', ['cssMin']);
+    gulp.watch('./assets/css/*.css', ['cssMin']);
     gulp.watch("./assets/js/*.js").on('change', browserSync.reload);;
     gulp.watch('./views/**/*.pug',['views']).on('change', browserSync.reload);
 
 });
 
-gulp.task('default', ["watch"]);
+//default
+gulp.task('default',['scripts','watch']);
